051effab3e180aba56194cf7ccd6f852c93c987c
Rasika Bharati <61646811+rasikanbharati@users.noreply.github.com>
2021-05-14 14:15:41 +0530

    Add files via upload

---
+++ practiceswiftui/practiceswiftui/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  practiceswiftui
+//
+//  Created by Ksheetij Tayde on 10/05/21.
+//  Copyright © 2021 rasika. All rights reserved.
+//
+
+import SwiftUI
+struct DataModel: Identifiable {
+let id: String
+let name: String
+let imageName: String
+}
+struct ContentView: View {
+let data: [DataModel] = [
+.init(id: "0", name: "SteveJobs", imageName: "SteveJobs"),
+.init(id: "1", name: "Satya Nadella", imageName: "Satya Nadella"),
+.init(id: "2", name: "Jeff Bezos", imageName: "Jeff Bezos"),
+.init(id: "3", name: "Tim Cook", imageName: "Tim Cook")
+]
+var body: some View {
+NavigationView {
+List {
+ForEach(data) { items in
+ForEach(0..<2) { item in
+CollectionView(data: items)
+  }
+}
+}.navigationBarTitle("CollectionView")
+   }
+ }
+}
+struct CollectionView: View {
+let data: DataModel
+var body: some View {
+VStack {
+HStack {
+ForEach(0..<2) { items in
+Spacer()
+Image(self.data.imageName)
+.resizable()
+.frame(width: 150, height: 150)
+.foregroundColor(.yellow)
+.clipShape(Circle())
+.shadow(radius: 10)
+Spacer()
+}.padding(.bottom, 16)
+}
+HStack {
+Spacer()
+Text(self.data.name)
+Spacer()
+Text(self.data.name)
+Spacer()
+     }
+   }
+ }
+}
+struct ContentView_Previews: PreviewProvider {
+static var previews: some View {
+ContentView().environment(\.colorScheme, .light)
+  }
+}
