e0567dac84b012f8fc629720a11b4175db75f0d9
Rasika Bharati <61646811+rasikanbharati@users.noreply.github.com>
2021-05-14 14:24:41 +0530

    Add files via upload

---
+++ FittnessApp/FittnessApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FittnessApp
+//
+//  Created by Ksheetij Tayde on 10/05/21.
+//  Copyright © 2021 rasika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoimage: UIImageView!
+    @IBOutlet weak var slidercollectionview: UICollectionView!
+    @IBOutlet weak var pageview: UIPageControl!
+    @IBOutlet weak var mycollectionview: UICollectionView!
+    @IBOutlet weak var celltype: UICollectionViewCell!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
